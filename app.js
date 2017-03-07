98d50b8ef96e5cc96b83240e56506553611109c8
Mansoorasad <mansoor11120@gmail.com>
2017-03-07 22:16:12 +0500

    JAVA SCRIPT 2

---
+++ app.js
@@ -0,0 +1,16 @@
+var a=+prompt("Enter your Final Marks");
+if(a>=80){
+    alert("Outstanding!! You got A+");
+}
+else if(a>=70){
+    alert("Congratulations!!! You got A grade");
+}
+else if(a>=60){
+    alert("Good!! You got B grade");
+} 
+else if(a>=50){
+    alert("You got C garde");
+}
+else{
+    alert("FAIL");
+}
